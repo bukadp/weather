85d1d4b511d72537f7672b98f88dba589051ff1a
bukadp <thedizain@gmail.com>
2022-01-04 14:09:58 +0200

    toggle favorite button

--- main.js
+++ main.js
@@ -19,8 +19,9 @@ window.onload = function (){
 
 
 for (let btn_delete of btn_deletes) {
-  btn_delete.addEventListener("click", DeleteCity)
+  btn_delete.addEventListener("click", DeleteCityFromAddedLocations)
 }
+
 
 let btn_favorite = document.querySelector(".tabcontent-now-fvorite-btn"); 
 const cityFavorites = document.querySelectorAll(".row__right-city");
@@ -37,31 +38,100 @@ for (let cityFavorite of cityFavorites) {
 function getCityName (){
   let cityName = document.querySelector(".city").value;
       fetchWeather (cityName);
+      fetchForecast(cityName);
 }
 
 
 function getCityNameFromFavorite(){
-  let cityName = document.querySelector(".city").value;
   cityName = this.textContent;
-  fetchWeather (cityName);
+  fetchWeather(cityName);
+  fetchForecast(cityName);
+  getFavoriteButtonColor(cityName);
+}
+
+
+// chek btn
+function getFavoriteButtonColor(cityName){
+  if (!checkOnFavorites(cityName)){
+    btn_favorite.classList.remove("active");  
+ }
+ else {
+  
+  btn_favorite.classList.add("active");
+ }
+
+}
+
+
+// true or false
+function checkOnFavorites(city){
+  let parsedFavoriteCities = JSON.parse(localStorage.getItem("favoriteCities"));
+    
+    let isExist =  parsedFavoriteCities.includes(city);
+
+    return isExist;
+}
+
+
+
+const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const serverUrlForecast = 'http://api.openweathermap.org/data/2.5/forecast';
+const serverIcoUrl = 'https://openweathermap.org/img/wn/';
+const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+
+
+function fetchForecast(cityName){
+  const urlForecast = `${serverUrlForecast}?q=${cityName}&appid=${apiKey}`;
+  fetch(urlForecast)
+  .then((response) => response.json())
+  .then (data => {
+
+    let resultForecastCity = document.querySelector(".tabcontent-forecast-city");
+    resultForecastCity.innerHTML = `${data.city.name}`;
+
+    let resultForecastDay = document.querySelector(".tabcontent-forecast-info-box-day"); 
+    resultForecastDay.innerHTML = `${data.list[0].dt}`;
+
+    let resultForecastTime = document.querySelector(".tabcontent-forecast-info-box-time"); 
+    resultForecastTime.innerHTML = `${data.list[0].dt}`;
+
+    let resultForecastTemp = document.querySelector(".tabcontent-forecast-info-box-temperature>span"); 
+    let numberTemp = +`${data.list[0].main.temp}`;
+    resultForecastTemp.innerHTML = Math.round(numberTemp-273);
+
+    let resultForecastFeelsLikeTemp = document.querySelector(".tabcontent-forecast-info-box-feels>span"); 
+    let numberFeelsLikeTemp = +`${data.list[0].main.feels_like}`;
+    resultForecastFeelsLikeTemp.innerHTML = Math.round(numberFeelsLikeTemp-273);
+
+
+    let resultForecastDescription = document.querySelector(".tabcontent-forecast-info-box-ico-name");
+    let resultForecastDescriptionValue = `${data.list[0].weather[0].main}`;
+    resultForecastDescription.innerHTML = `${resultForecastDescriptionValue[0].toUpperCase()}${resultForecastDescriptionValue.slice(1)}`;
+
+
+    let resultNowIco = document.querySelector(".tabcontent-forecast-info-box-ico-icon"); 
+    let icoNumber = `${data.list[0].weather[0].icon}`;
+    let ico = document.createElement('img');
+    ico.src = `${serverIcoUrl}${icoNumber}.png`;
+        
+    resultNowIco.append(ico);
+    let resultNowIcoFirst = document.querySelector(".tabcontent-forecast-info-box-ico-icon>img"); 
+    resultNowIcoFirst.remove();
+
+  })
+  
 }
 
 
 function fetchWeather (cityName){
-  const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-  const serverIcoUrl = 'https://openweathermap.org/img/wn/';
-  //const cityName = document.querySelector(".city").value;
-  const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
   const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
-  
-  
-
+ 
   fetch(url)
   .then((response) => response.json())
   .then (data => {
     
     let resultNowCity = document.querySelector(".tabcontent-now-city, .tabcontent-details-city"); 
-    resultNowCity.innerHTML = `${data.name}`
+    resultNowCity.innerHTML = `${data.name}`;
 
 
 
@@ -70,10 +140,11 @@ function fetchWeather (cityName){
     let numberTemp = +`${data.main.temp}`;
     resultNowTemp.innerHTML = Math.round(numberTemp-273);
     
+
+
     let resultNowIco = document.querySelector(".tabcontent-now-ico"); 
     let icoNumber = `${data.weather[0].icon}`;
     let ico = document.createElement('img');
-    /**ico.src = 'https://openweathermap.org/img/wn/04n@2x.png';**/
     ico.src = `${serverIcoUrl}${icoNumber}@4x.png`;
         
     resultNowIco.append(ico);
@@ -93,13 +164,34 @@ function fetchWeather (cityName){
 
     let resultSunrise = document.querySelector(".tabcontent-details-sunrise>span"); 
     resultSunrise.innerHTML = `${data.sys.sunrise}`
-    AddCurrentCityInLocalStorage(data.name)
-    //
+    
+
+    let resultSunset = document.querySelector(".tabcontent-details-sunset>span"); 
+    resultSunset.innerHTML = `${data.sys.sunset}`
+
+    let resultFeelsLike = document.querySelector(".tabcontent-details-feels>span"); 
+    resultFeelsLike.innerHTML = `${data.main.feels_like}`
+
+    let resultWeatherDescription = document.querySelector(".tabcontent-details-weather>span");
+    let resultWeatherDescriptionValue = `${data.weather[0].description}`;
+    resultWeatherDescription.innerHTML = `${resultWeatherDescriptionValue[0].toUpperCase()}${resultWeatherDescriptionValue.slice(1)}`;
+    
+    HandlerFetch(data.name);
+
+    //let city = document.querySelector(".city").value;
+    //getFavoriteButtonColor (city);
+  
 //вот тут КОНЕЦ
     })
     .catch(error => alert(error.message));
-     //.finally(() => {UI_FORM_SEARCH.reset()});
+     
   }
+
+function  HandlerFetch (dataName){
+  getFavoriteButtonColor(dataName);
+  AddCurrentCityInLocalStorage(dataName);
+
+}
 
   function AddCurrentCityInLocalStorage(nameCity){
     localStorage.setItem("CurrentCity", nameCity);
@@ -107,72 +199,82 @@ function fetchWeather (cityName){
 
   
 
-  function DeleteCity(){
+  function DeleteCityFromAddedLocations(){
     this.parentElement.remove();
     let delCity = this.parentElement.querySelector(".row__right-city").innerText;
     
     let parsedFavoriteCities = JSON.parse(localStorage.getItem("favoriteCities"));
-    console.log (parsedFavoriteCities)
-    localStorage.removeItem(delCity);
+    
+    let updatedFavoriteCities =  parsedFavoriteCities.filter(function (el){
+      return el !== delCity;
+    })
+    localStorage.setItem("favoriteCities", JSON.stringify(updatedFavoriteCities));
+    favoriteCities = updatedFavoriteCities;
+    
  }
 
 
-  btn_favorite.addEventListener("click", HandleFavoriteClick)
-  //btn_favorite.addEventListener("click", Store)
-  //btn_favorite.addEventListener("click", ChangeBtn)
-
+  btn_favorite.addEventListener("click", isNew)
+  
 
   /**function ChangeBtn (){
     
-    btn_favorite.classList.add("active");
+    btn_favorite.classList.toggle("active");
   }**/
 
 
   //localStorage
 
+  function isNew(){
+    btn_favorite.classList.toggle('active')
+    
+    if (btn_favorite.classList.contains('active')){
+      HandleFavoriteClick();
+    }
+    else{
+      DeleteCity();
+    }
+  }
+
+function DeleteCity(){
   
+  let resultNowCity = document.querySelector(".tabcontent-now-city").textContent;
 
+  let arrFavoriteCities = document.getElementsByClassName("row__right-city");
+  
+  let deleteFavoriteCities; 
+  for (let i=0; i<arrFavoriteCities.length; i++){
+    if (resultNowCity==arrFavoriteCities[i].textContent){
+      deleteFavoriteCities = arrFavoriteCities[i];
+    }
+    
+  }
 
-
-/**
-  const city = new Map([
-    ['Nur-Sultan', true],
-    ['Amur', true],
-    ['Samara', true],
-    ['Bali', true],
-    ['Dane', true],
-    ['Kilo', true],
-  ]);
-
- 
-function AddCityArr(){
-  let resultNowCity = document.querySelector(".tabcontent-now-city").textContent; 
-  city.set(resultNowCity, true);
-  alert(city.size);
+  deleteFavoriteCities.parentElement.remove();
+  
+  
+  let parsedFavoriteCities = JSON.parse(localStorage.getItem("favoriteCities"));
+  
+  let updatedFavoriteCities =  parsedFavoriteCities.filter(function (el){
+    return el !== resultNowCity;
+  })
+  localStorage.setItem("favoriteCities", JSON.stringify(updatedFavoriteCities));
+  favoriteCities = updatedFavoriteCities;
+  
 }
-**/
+
 
 function HandleFavoriteClick(){
+
+  
   AddCity(); 
   //Store();  
   AddFavoriteCities();
+ // ChangeBtn ();
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////ТУТ ПРОДОЛЖАТЬ!!!
-/**function Store(){ 
-
-    let favoriteCitiesUi = this.parentElement.querySelector(".row__right-city").innerText;
-    let cities = [];
-    localStorage.setItem(array, JSON.stringify(array));
-    array = JSON.parse(localStorage.getItem("array"));
-  
-    console.log(typeof array); // объект
-    console.log(array); // [1, 2, 3]
-
-
-    /// считать массив JSON.parse(localStorage.getItem("data");
-  }**/
 
 
   let favoriteCities = [];
@@ -184,7 +286,7 @@ function HandleFavoriteClick(){
      favoriteCities.push(resultNowCity);
 
     localStorage.setItem("favoriteCities", JSON.stringify(favoriteCities));
-
+    
     }
   }
   
@@ -226,7 +328,7 @@ function HandleFavoriteClick(){
 function CheckCityInLocalStorage(resultNowCity){
   let result = false;
   let parsedFavoriteCities = JSON.parse(localStorage.getItem("favoriteCities"));
-  //let resultNowCity = document.querySelector(".tabcontent-now-city").textContent;
+  
   if (!!parsedFavoriteCities){
     result = parsedFavoriteCities.includes(resultNowCity);
   }
@@ -255,6 +357,7 @@ function AddCity(){
   let resultNowCity = document.querySelector(".tabcontent-now-city").textContent;
   if (!checkToExistCity(resultNowCity)){
      btn_favorite.classList.add("active");
+     
   }
   if (!checkToExistCityInTheList(resultNowCity)){
     let cityFavorite = document.createElement('li'); 
@@ -264,7 +367,7 @@ function AddCity(){
           <button class="row__right-city">${resultNowCity}</button>
           <button class="row__right-close" data-close="close"></button>
           `;
-    document.querySelector(".row__right-close").addEventListener("click", DeleteCity);
+    document.querySelector(".row__right-close").addEventListener("click", DeleteCityFromAddedLocations);
     document.querySelector(".row__right-city").addEventListener("click", getCityNameFromFavorite);
   }
 }
